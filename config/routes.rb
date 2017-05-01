2cbe784d030556a8255d241d29ba792bf1af03a5
Jeong Hoon Koo <junghoone17@gmail.com>
2017-05-01 00:04:29 -0400

    completed models & first draft of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'categories#index'
+
+  resources :categories
+
+  resources :recipes do
+    resources :reviews, except: [:index, :show]
+    resources :directions, except: [:index, :show]
+    resources :categories, except: [:index, :show]
+  end
+
+  resources :recipes, only: [:index, :show] do
+    member do
+      post 'add_will_try_connecting'
+      delete 'remove_will_try_connecting'
+      post 'add_did_try_connecting'
+      delete 'remove_did_try_connecting'
+    end
+  end
+
+
+
 end
